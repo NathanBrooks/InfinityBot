0d96a1c2364dd9ca607eeed5a4a1847adda2e91d
NathanBrooks <nathan.brooks@eagles.ewu.edu>
2017-02-14 14:20:29 -0800

    forgot to add the file in previous commit

---
+++ bot_modules/ConfirmModule.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const module_name = "Confirmation Module"
+const module_version = "0.1"
+const module_settings = "/ConfirmationModule"
+
+var api;
+var app;
+
+module.exports = {
+	moudle_name: module_name,
+	module_version: module_version,
+	module_settings: module_settings,
+
+    init: function(parent_api, parent_app) {
+        api = parent_api;
+        app = parent_app;
+
+        api.on('message', handleMessage);
+    },
+
+    free: function() {
+        api.removeListener('message', handleMessage);
+
+        api = null;
+        app = null;
+    }
+};
+
+var inc = 0;
+
+
+
+
+function handleMessage(message){
+        if('text' in message) {
+        if(message.text[0] == '/') {
+            parseCommand(message);
+        }
+    }
+}
+
+function parseCommand(message){
+    var paramList = message.text.split(/\s+/);
+    var fullCommand = paramList[0].split(/@/);
+    if(fullCommand.length == 1 ||                                   // no name was specified
+       fullCommand[1].toLowerCase() == global.BotName.toLowerCase()) {     // check if the command was meant for us
+
+        switch(fullCommand[0]) {
+            case "/confirm":
+                confirmed(message);
+                break;
+            default: ;
+        }
+    }
+}
+
+
+function confirmed(message){
+    if('reply_to_message' in message) {
+        global.SendMessage("I can confirm this!", message.chat.id, message.reply_to_message.message_id);
+    } else {
+        global.SendMessage("I can confirm this!", message.chat.id, message.message_id);
+    }
+}
